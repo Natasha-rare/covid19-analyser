306ca88956c1ce8b22ec49ba3830096430b81303
Natasha-rare <Natasha_ea5@mail.ru>
2021-02-14 20:40:33 +0300

    using second screen

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFileDialog
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFileDialog, QLabel
 from PIL import Image
 import shutil
 import os
 import torch
 import requests, cv2, tqdm, torchvision, yaml, matplotlib, pandas, seaborn
+
+
+class CreateMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('mainPage.ui', self)
 
 
 class MyWidget(QMainWindow):
@@ -17,7 +24,8 @@ class MyWidget(QMainWindow):
         self.loadBtn.clicked.connect(self.load)
         self.homeBtn.clicked.connect(self.home)
         # self.settingsBtn.clicked.connect(self.settings)
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path_or_model=f'{os.getcwd()}/best.pt')
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom',
+                path_or_model=f'{os.getcwd()}/best.pt')
 
     def home(self):
         uic.loadUi('covid.ui', self)
@@ -34,18 +42,41 @@ class MyWidget(QMainWindow):
         # TODO: check file type. If it is NIFTI, convert to PNG
         self.analyze(filenames)
 
-    def analyze(self, images):
-        imgs = []
-        for img in images:
-            imgs.append(Image.open(img))
-        results = self.model(imgs, size=256)
+    def analyze(self, filenames):
+        images = []
+        for img in filenames:
+            images.append(Image.open(img))
+        results = self.model(images, size=256)
         results.print()
         if not os.path.exists('results'):
             os.mkdir('results')
-        results.save(save_dir='results')
+        results.save()
+        self.openResults(filenames)
 
+    def illness_check(self, image_path):
+        img = cv2.imread(f'{os.getcwd()}/results/{image_path}')
+        if len(img.shape) < 3:
+            return False
+        if img.shape[2] == 1:
+            return False
+        b, g, r = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+        if (b == g).all() and (b == r).all():
+            return False
+        return True  # if not grayscale
+
+    def openResults(self, images):
+        main = uic.loadUi('mainPage.ui', self)
+        main.show()
+        print('opened')
+        # self.close()
+
+        for im in images:
+            imageLabel = QLabel()
+            imageLabel.setPixmap(QPixmap.fromImage(QImage(im)))
+            is_ill = self.illness_check(im)
+            main.srollResults.setWidget(imageLabel)
 
 app = QApplication(sys.argv)
 ex = MyWidget()
-# ex.show()
+ex.show()
 sys.exit(app.exec_())
