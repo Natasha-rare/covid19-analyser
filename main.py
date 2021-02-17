95775e887d7e2f58558d8cdb1e456f3190ef9b7a
Daniel Khromov <danielkhromov@gmail.com>
2021-02-17 21:42:16 +0300

    py2app (hope it works)

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
 if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
     PyQt5.QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-if getattr(sys, 'frozen', False):
-    # If the application is run as a bundle, the PyInstaller bootloader
-    # extends the sys module by a flag frozen=True and sets the app
-    # path into variable _MEIPASS'.
-    application_path = sys._MEIPASS
-else:
-    application_path = os.path.dirname(os.path.abspath(__file__))
+# if not getattr(sys, 'frozen', False):
+#     application_path = os.path.dirname(os.path.abspath(__file__))
+# else:
+#     # If the application is run as a bundle, the PyInstaller bootloader
+#     # extends the sys module by a flag frozen=True and sets the app
+#     # path into variable _MEIPASS'.
+#     application_path = sys._MEIPASS
+application_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_path(file=None):
@@ -137,7 +138,6 @@ class MainWindow(QMainWindow):
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        print(application_path)
         uic.loadUi(get_path('covid.ui'), self)
 
         self.loadBtn.clicked.connect(self.load)
