7c99f4d6bb7116714a8e520153a7c83e44b590de
Daniel Khromov <danielkhromov@gmail.com>
2021-02-18 10:52:16 +0300

    win

---
+++ win_setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    console=['main.py'],
+    # packages=['PyQt5',
+    #           'PIL',
+    #           'torch',
+    #           'pandas',
+    #           'requests',
+    #           'cv2',
+    #           'tqdm',
+    #           'torchvision',
+    #           'yaml',
+    #           'matplotlib',
+    #           'seaborn']
+)
